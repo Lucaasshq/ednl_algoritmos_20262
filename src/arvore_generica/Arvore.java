bccf5617908031586d7cf06335c4e1b81ea442af
Your Name <you@example.com>
2026-08-27 00:51:54 +0000

    incrementando os métodos da árvore genérica

--- src/arvore_generica/Arvore.java
+++ src/arvore_generica/Arvore.java
@@ -30,27 +30,113 @@ public class Arvore<T> {
 	public Arvore(T dadoRaiz){
 		this.raiz = new No<>(dadoRaiz);
 	}
+
+	// construtor de árvore vazia
+	public Arvore(){
+		this.raiz = null;
+	}
+
+	// verifica se a árvore é vazia
+	public boolean arvoreVazia(){
+		if(this.raiz == null)
+			return true;
+		return false;
+	}
+
+	// criar uma raiz para a árvore vazia
+	public void criarRaiz(No<T> raiz){
+		if(this.arvoreVazia()) {
+			this.raiz = raiz;
+		}
+	}
+
+	// retorna a raiz da árvore
+	public No<T> obterRaiz() {
+		return this.raiz;
+	}
 	
+	// insere um filho com a chave "dadoFilho" em "pai"
 	public void inserirNo(No<T> pai, T dadoFilho){
 		if(pai != null){
 			No<T> noFilho = new No<>(dadoFilho);
 			pai.inserirFilho(noFilho);
 		}
 	}
+
+	public void inserirNo(No<T> pai, No<T> filho){
+		if(pai != null){
+			pai.inserirFilho(filho);
+		}
+	}
+
+	public int altura(){
+		if(this.arvoreVazia())
+			return -1;
+		else {
+			return this.alturaNo(this.raiz);
+		}
+	}
 	
+	private int alturaNo(No<T> no){
+		int maiorAltura = -1;
+		
+		for(No<T> filho: no.filhos){
+			int alturaFilho = alturaNo(filho);
+			if(alturaFilho > maiorAltura)
+				maiorAltura = alturaFilho;
+		}
+		
+		return maiorAltura + 1;
+	}
+
+	public No<T> buscar(T dado){
+		if(this.arvoreVazia()){
+			return null;
+		}else {
+			return buscarNo(this.raiz, dado);
+		}
+	}
+
+	private No<T> buscarNo(No<T> noAtual, T dado){
+		if(noAtual.dado == dado){ // nó contém o dado
+			return noAtual;
+		}else { // procurar o dado nos filhos do nó atual
+			for(No<T> filho : noAtual.filhos) {
+				No<T> no = buscarNo(filho, dado);
+				if(no != null)
+					return no;
+			}
+		}
+		return null; // dado não encontrado!
+	}
+
+	// TODO
+	public No<T> retornarPai(T dado){
+		//...
+		//return retornarPai(this.raiz, dado);
+		return null;
+	}
+	
+	// TODO
+	private No<T> retornarPai(No<T> noAtual, T dado){
+		// verificar se existe algum filho do "NoAtual" que é "dado".
+		//return noAtual;
+		// senão, para cada filho do noAtual ...
+		return null;
+	}
+
 	public void imprimir() {
-		imprimirRecursivo(this.raiz);
+		imprimirRecursivo(this.raiz,"");
 		System.out.println("");
 	}
 	
-	private void imprimirRecursivo(No<T> noAtual){
+	private void imprimirRecursivo(No<T> noAtual, String recuo){
 		if(noAtual == null) return;
 		
-		System.out.print(noAtual.dado + " ");
+		System.out.println(recuo + "+- " + noAtual.dado);
 		
 		for(No<T> filho: noAtual.filhos){
-				imprimirRecursivo(filho);
+				imprimirRecursivo(filho, recuo + " ");
 		}
-	
 	}
 }
--- src/arvore_generica/Main.java
+++ src/arvore_generica/Main.java
@@ -1,13 +1,37 @@
 public class Main {
 	public static void main(String[] args) {
-		Arvore<String> arvore = new Arvore<>("A");
+		Arvore<String> arvore = new Arvore<>();
+		Arvore<String> arvore_b = new Arvore<>();
+		Arvore<String> arvore_c = new Arvore<>();
 		
-		arvore.inserirNo(arvore.raiz,"B");
-		arvore.inserirNo(arvore.raiz,"C");
+		No<String> no_a = new No<>("A");
+		arvore.criarRaiz(no_a);
+
+		No<String> no_b = new No<>("B");
+		No<String> no_c = new No<>("C");
+		No<String> no_d = new No<>("D");
+		No<String> no_e = new No<>("E");
 		
-		No<String> no_b = arvore.raiz.filhos.get(0);
-		arvore.inserirNo(no_b,"D");
+		arvore.inserirNo(no_a,no_b);
+		arvore.inserirNo(no_a,no_c);
+		arvore.inserirNo(no_b,no_d);
+		arvore.inserirNo(no_b,no_e);
 		
 		arvore.imprimir();
+		System.out.println("Altura: " + arvore.altura());
+		System.out.println("Resultado (buscar - D): " + arvore.buscar("D"));
+		System.out.println("Resultado (buscar - F): " + arvore.buscar("F") + "\n");
+
+		// árvore somente com a raiz
+		No<String> no_f = new No<>("F");
+		arvore_b.criarRaiz(no_f);
+
+		arvore_b.imprimir();
+		System.out.println("Altura: " + arvore_b.altura());
+
+		// árvore vazia
+		arvore_c.imprimir();
+		System.out.println("Altura: " + arvore_c.altura());
+
 	}
 }
